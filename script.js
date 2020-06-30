1ac7f56c5b121d24fe30d7e7bcda2ec080f10eca
andy <andy.ukpe@outlook.com>
2020-06-30 16:50:30 +0200

    aggiunta correzioni

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ $(document).ready(function () {
 
   $('.month').html(date.format('MMMM YYYY'));
 
+  var source = $("#calendario").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 1; i <= days; i++) {
+    // var month = moment("2018-01-01").format('MMMM');
+    // var day = i + ' ' + month;
+    var currentDate = moment(date).add(1, 'days')
+    console.log(currentDate);
+
+    var context = { day: currentDate};
+    var html = template(context);
+    $('.container').append(html);
+    }
+
   $.ajax(
     {
       url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
@@ -22,17 +36,8 @@ $(document).ready(function () {
 
     );
 
-    var source = $("#calendario").html();
-    var template = Handlebars.compile(source);
 
-    for (var i = 1; i <= days; i++) {
-      var month = moment("2018-01-01").format('MMMM');
-      var day = i + ' ' + month;
 
-      var context = { day: day};
-      var html = template(context);
-      $('.container').append(html);
-    }
 
 
 
